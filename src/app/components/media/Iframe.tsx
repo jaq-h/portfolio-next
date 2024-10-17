f97c0d945164fe0f819faa24aaa22a1a43049b9c
jaq-h <jaq-h@github.com>
2024-10-17 00:29:46 -0500

    auto width

--- src/app/components/media/Iframe.tsx
+++ src/app/components/media/Iframe.tsx
@@ -1,12 +1,12 @@
 'use client';
 export type IframeProps = {
     src: string;
-    width: number;
+    width?: number;
     height: number;
 }
 
 export default function Iframe(props: IframeProps) {
     return (
-        <iframe src={props.src} width={props.width} height={props.height} allowFullScreen />
+        <iframe src={props.src} width={props.width ? props.width : "100%"} height={props.height} allowFullScreen />
     )
 }
